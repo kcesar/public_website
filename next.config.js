a2fbea6eba734f92237fd833eb4d46d531194cf8
kcesar-deploy <30479300+kcesar-deploy@users.noreply.github.com>
2024-11-30 19:04:49 -0800

    Deploy to Azure GitHub action

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone',
     images: {
         remotePatterns: [
             {
